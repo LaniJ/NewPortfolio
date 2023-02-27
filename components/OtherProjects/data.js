4dd58ff4fd312215d49f8f9b35efd47421705477
Lani <ooluwalani@gmail.com>
2023-02-27 16:05:01 -0500

    Adds project list section

---
+++ components/OtherProjects/data.js
@@ -0,0 +1,38 @@
+export const projectList = [
+  {
+    projectName: "AQ Identity Verification",
+    link: "https://stagingaq.enyata.com/",
+    cardContent: "A nicer look at your GitHub profile and repo stats. Includes data visualizations of your top languages, starred repositories, and sort through your top repos by number of stars, forks, and size.",
+    // altText: ""
+  },
+  {
+    projectName: "Simple Video Player",
+    link: "https://simplevideoplayer.com/",
+    cardContent: "A nicer look at your GitHub profile and repo stats. Includes data visualizations of your top languages, starred repositories, and sort through your top repos by number of stars, forks, and size."
+  },
+  {
+    projectName: "Todo App",
+    link: "https://todo-ezt5mf06k-lanij.vercel.app/",
+    cardContent: "A nicer look at your GitHub profile and repo stats. Includes data visualizations of your top languages, starred repositories, and sort through your top repos by number of stars, forks, and size."
+  },
+  {
+    projectName: "React Calculator",
+    link: "https://react-calc-nine.vercel.app/",
+    cardContent: "A nicer look at your GitHub profile and repo stats. Includes data visualizations of your top languages, starred repositories, and sort through your top repos by number of stars, forks, and size."
+  },
+  {
+    projectName: "Vue3 App",
+    link: "",
+    cardContent: "A nicer look at your GitHub profile and repo stats. Includes data visualizations of your top languages, starred repositories, and sort through your top repos by number of stars, forks, and size."
+  },
+  {
+    projectName: "Card Flip game",
+    link: "",
+    cardContent: "A nicer look at your GitHub profile and repo stats. Includes data visualizations of your top languages, starred repositories, and sort through your top repos by number of stars, forks, and size."
+  },
+  {
+    projectName: "Parallax Page",
+    link: "https://parallax-landing-page-theta.vercel.app/",
+    cardContent: "A nicer look at your GitHub profile and repo stats. Includes data visualizations of your top languages, starred repositories, and sort through your top repos by number of stars, forks, and size."
+  },
+]
--- components/OtherProjects/index.jsx
+++ components/OtherProjects/index.jsx
@@ -1,60 +1,42 @@
 import styles from '../../styles/OtherProjects.module.css'
+import Image from 'next/image'
+import folderIcon from '../../assets/icons/folder-icon.svg'
+import newTabIcon from '../../assets/icons/new-tab.svg'
+import { projectList } from './data'
+
 
 const OtherProjects = () => {
+ 
   return ( 
     <div>
       <h3 className="sectionHeader">Other Noteworthy Projects</h3>
       <div className={styles.project_section}>
-        <div className={styles.project_card}>
-          <div>icons</div>
-          <div>
-            <h3 className={styles.title}>Building RIch accessible web applications</h3>
-            <p className={styles.body}>A nicer look at your GitHub profile and repo stats. Includes data visualizations of your top languages, starred repositories, and sort through your top repos by number of stars, forks, and size.</p>
-          </div>
-          <div>
-            <ul>
-              <li>Vue</li>
-              <li>chart.js</li>
-              <li>Firebase</li>
-            </ul>
-          </div>
-        </div>
-
-        <div className={styles.project_card}>
-          <div>icons</div>
-          <div>
-            <h3 className={styles.title}>Building RIch accessible web applications</h3>
-            <p className={styles.body}>A nicer look at your GitHub profile and repo stats. Includes data visualizations of your top languages, starred repositories, and sort through your top repos by number of stars, forks, and size.</p>
-          </div>
-          <div>
-            <ul>
-              <li>Vue</li>
-              <li>chart.js</li>
-              <li>Firebase</li>
-            </ul>
-          </div>
-        </div>
-
-        <div className={styles.project_card}>
-          <div>icons</div>
-          <div>
-            <h3 className={styles.title}>Building RIch accessible web applications</h3>
-            <p className={styles.body}>A nicer look at your GitHub profile and repo stats. Includes data visualizations of your top languages, starred repositories, and sort through your top repos by number of stars, forks, and size.</p>
-          </div>
-          <div>
-            <ul>
-              <li>Vue</li>
-              <li>chart.js</li>
-              <li>Firebase</li>
-            </ul>
-          </div>
-        </div>
+          {projectList.map(({projectName, link, cardContent}) => (
+            <div className={styles.project_card}>
+              <div className={styles.project__card__icons}>
+                <Image
+                  src={folderIcon}
+                  alt="folderIcon" 
+                />
+                <a href={link} target="_blank" rel="noopener noreferrer">
+                  <Image
+                    src={newTabIcon}
+                    alt="newTabIcon" 
+                  />
+                </a>
+              </div>
+              <div>
+                <h3 className={styles.title}>{projectName}</h3>
+                <p className={styles.body}>{cardContent}</p>
+              </div>
+            </div>
+          ))}
+       
         {/* https://stagingaq.enyata.com/ */}
         {/* simple video player */}
         {/* vue image game */}
         {/* vue3 project */}
-        {/* todo app */}
-        {/* calculator */}
+        
       </div>
       
     </div>
