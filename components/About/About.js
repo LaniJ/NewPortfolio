56b3db70dbb792b3d046a3243d997884a0ad5fa2
Lani <ooluwalani@gmail.com>
2023-02-28 11:15:39 -0500

    escaped apostrophe

--- components/About/About.js
+++ components/About/About.js
@@ -8,7 +8,7 @@ const About = () => {
         <h3 className="sectionHeader">About Me</h3>
         <p className={styles.text}>Hello! My name is Lani, an Architect turned software engineer, who  has spent the past three years reveling in the joy of converting UI designs to pixel-perfect applications.
         </p>
-        <p className={styles.text}>I&apos;m bilingual, and I love learning new languages. My interest in programming languages piqued when I was searching for a new language to learn, and that's when I stumbled upon Python. Since then, I&apos;v been passionate about programming and continuously expanding my knowledge in the field.
+        <p className={styles.text}>I&apos;m bilingual, and I love learning new languages. My interest in programming languages piqued when I was searching for a new language to learn, and that&apos;s when I stumbled upon Python. Since then, I&apos;v been passionate about programming and continuously expanding my knowledge in the field.
         </p>
         <Skills />
       </div>
