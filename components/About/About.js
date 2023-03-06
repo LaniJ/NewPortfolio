6b39b71fbfbdba03f4df24a41de6d4ba61dde74b
Lani <ooluwalani@gmail.com>
2023-03-06 16:05:52 -0500

    font size modifications

--- components/About/About.js
+++ components/About/About.js
@@ -6,9 +6,9 @@ const About = () => {
     <div className={`${styles.about} fade-up`} id="about">
       <div className={styles.textSection}>
         <h3 className="sectionHeader">About Me</h3>
-        <p className={styles.text}>Hello! My name is Lani, an Architect turned software engineer, who  has spent the past three years reveling in the joy of converting UI designs to pixel-perfect applications.
+        <p className={`${styles.text} leading-[33px]`}>Hello! My name is Lani, an Architect turned software engineer, who  has spent the past three years reveling in the joy of converting UI designs to pixel-perfect applications.
         </p>
-        <p className={styles.text}>I&apos;m bilingual, and I love learning new languages. My interest in programming languages piqued when I was searching for a new language to learn, and that&apos;s when I stumbled upon Python. Since then, I&apos;v been passionate about programming and continuously expanding my knowledge in the field.
+        <p className={`${styles.text} leading-[33px]`}>I&apos;m bilingual, and I love learning new languages. My interest in programming languages piqued when I was searching for a new language to learn, and that&apos;s when I stumbled upon Python. Since then, I&apos;ve been passionate about programming and continuously expanding my knowledge in the field.
         </p>
         <Skills />
       </div>
--- components/Intro/Intro.js
+++ components/Intro/Intro.js
@@ -6,7 +6,7 @@ const Intro = () => {
       <p className="text-lg text-accent mb-8">Hi, My name is</p>
       <h1 className={`${styles.name} md:text-lg`}>Lani Oluwajuyitan</h1>
       <h1 className={`${styles.name} ${styles.role}`}>I build things for the web</h1>
-      <p className="text-lg max-w-full md:max-w-[53%] leading-[33px]">I am a frontend engineer who enjoys converting UI designs to pixel-perfect web applications. Coming from a background in Architecture and Interior design, I have a keen eye for the killer duo of <span className='text-accent'>functionality and aesthetics,</span> which is transferred to my work as a Front-end engineer and has made it easy to focus on user experience and ensuring that  applications have maximum accessibility.</p>
+      <p className={`${styles.text} text-lg max-w-full md:max-w-[53%] leading-[33px]`}>I am a frontend engineer who enjoys converting UI designs to pixel-perfect web applications. Coming from a background in Architecture and Interior design, I have a keen eye for the killer duo of <span className='text-accent'>functionality and aesthetics,</span> which is transferred to my work as a Front-end engineer and has made it easy to focus on user experience and ensuring that  applications have maximum accessibility.</p>
     </div>
    );
 }
